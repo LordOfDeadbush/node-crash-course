7ea21997818ed607098a517e3b2fd499a922f8ab
LordOfDeadbush <jcorn2012@gmail.com>
2024-01-22 13:50:40 -0800

    finished video 8

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 
 // express app
 const app = express(); 
@@ -9,6 +10,18 @@ app.set('views','myview')
 
 // listen for requests
 app.listen(3000);
+
+app.use(morgan('dev'));
+
+app.use((req, res, next) => {
+    console.log('new request made:');
+    console.log('host: ', req.hostname);
+    console.log('path: ', req.path);
+    console.log('method: ', req.method);
+    next();
+});
+
+
 
 app.get('/', (req, res) => {
     res.render('index');
